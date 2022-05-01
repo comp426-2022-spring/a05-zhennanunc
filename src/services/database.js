e38b47bae4aa0f0c6c54950bf3907622f961abbb
zhennanunc <zhennan@live.unc.edu>
2022-04-30 20:29:43 -0400

    test_john

--- src/services/database.js
+++ src/services/database.js
@@ -1,1 +1,38 @@
-// Put your database code here
+const database = require('better-sqlite3')
+
+const fs = require('fs');
+const datadir = './data/';
+
+if (!fs.existsSync(datadir)){
+    fs.mkdirSync(datadir);
+}
+
+const logdb = new database(datadir+'log.db')
+
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
+let row = stmt.get();
+if (row === undefined) {
+    console.log('Log database appears to be empty. Creating log database...')
+
+    const sqlInit = `
+        CREATE TABLE accesslog ( 
+            id INTEGER PRIMARY KEY, 
+            remoteaddr TEXT,
+            remoteuser TEXT,
+            time TEXT,
+            method TEXT,
+            url TEXT,
+            protocol TEXT,
+            httpversion TEXT,
+            status TEXT, 
+            referrer TEXT,
+            useragent TEXT
+        );
+    `
+
+    logdb.exec(sqlInit)
+} else {
+    console.log('Log database exists.')
+}
+
+module.exports = logdb
